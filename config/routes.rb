e1149c6249d7970d966f38688f0ba8484536ec50
ducbka <trungducphan95@gmail.com>
2016-10-25 15:17:41 +0700

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :users
+  resources :users 
   resources :account_activations, only: :edit
   resources :password_resets, except: [:show, :index, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy, :index]
 end
